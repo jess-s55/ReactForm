cdd60aceeac0f7275156092c4dd836628dec8b53
isaac <isaacsmith2120@gmail.com>
2023-07-25 11:56:50 -0500

    removed typo in api link preventing us from using it

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,41 +1,52 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 export default function SignUpForm() {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState(null);
-    
-    async function handleSubmit(e) {
-        e.preventDefault();
-        console.log("hello");
-        try {
-            const response = await fetch('https://fsa-jwt-practice.herokuapp.com/signup.', {
-                method: "POST",
-                body: JSON.stringify({ username, password }),
-            });
-            
-            const result = await response.json();
-            // console.log(result);
-        } catch (error) {
-            // console.error(error)
-            setError(error.message);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    console.log("hello");
+    try {
+      const response = await fetch(
+        "https://fsa-jwt-practice.herokuapp.com/signup",
+        {
+          method: "POST",
+          body: JSON.stringify({ username, password }),
         }
-        setUsername("");
-        setPassword("");
+      );
+
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      // console.error(error)
+      setError(error.message);
     }
-    return (
-        <div>
-            <h2>Sign Up</h2>
-            {error && <p>{error}</p>}
-            <form method="POST" onSubmit={handleSubmit}>
-                <label>
-                    Username:{""} <input value={username} onChange={(e) => setUsername(e.target.value)}/>
-                </label>
-                <label>
-                    Password:{""} <input value={password} onChange={(e) => setPassword(e.target.value)}/>
-                </label>
-                <button >Submit</button>
-            </form>
-            </div>
-    )
+    setUsername(" ");
+    setPassword(" ");
+  }
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      {error && <p>{error}</p>}
+      <form method="POST" onSubmit={handleSubmit}>
+        <label>
+          Username:{""}
+          <input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:{""}
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
